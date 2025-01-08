68afcc473bdc028520df2b5ac15b1e75e69fcb53
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2025-01-08 21:27:08 +0200

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,86 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, {Children, useCallback, useRef, useState} from "react";
+import {Canvas, Group, PaintStyle, Path, Skia, SkPaint, SkPath} from "@shopify/react-native-skia";
+import {SafeAreaView, StyleSheet, Text, useWindowDimensions} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+interface IPath {
+	path: SkPath;
+	paint: SkPaint;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const paint = () => {
+	const paint = Skia.Paint();
+	paint.setStyle(PaintStyle.Stroke);
+	paint.setStrokeWidth(4);
+	paint.setColor(Skia.Color('#606'))
+
+	return paint;
+}
+const App = () => {
+	const {height, width} = useWindowDimensions()
+
+	const styles = StyleSheet.create({
+		container: {
+			paddingVertical: '10%',
+			flex: 1,
+		},
+	})
+
+	const r = width * 0.33;
+	let [text, setText] = useState("FUCK");
+	const [paths, setPaths] = useState<IPath[]>([]);
+	const currentPath = useRef<SkPath | null>(null)
+	const onTouchStart = (event: any) => {
+		const {locationX, locationY} = event.nativeEvent;
+		currentPath.current = Skia.Path.Make()
+		currentPath.current.moveTo(locationX, locationY)
+		currentPath.current.lineTo(locationX, locationY)
+	}
+	const onTouchMove = (event: any) => {
+		if (!currentPath.current) return;
+
+		const {locationX, locationY} = event.nativeEvent;
+		currentPath.current?.lineTo(locationX, locationY)
+	}
+	const onTouchEnd = useCallback(() => {
+		if (!currentPath.current) return;
+		console.log(currentPath.current, paint());//vla
+
+		setPaths((values) => values.concat({
+			path: currentPath.current!,
+			paint: paint()
+		}))
+
+		currentPath.current = null;
+	}, [currentPath])
+
+	return (
+		<SafeAreaView style={styles.container} >
+			<Text >{text}</Text >
+			<Canvas style={styles.container}
+					onTouchStart={onTouchStart}
+					onTouchMove={onTouchMove}
+					onTouchEnd={onTouchEnd}
+					onTouchCancel={onTouchEnd}
+			>
+				{!!currentPath.current && <Path
+					path={currentPath.current!}
+					paint={paint()}
+				/>}
+				<Group blendMode="multiply" >
+					{
+						Children.toArray(
+							paths.map((value) => (
+								<Path
+									path={value.path}
+									paint={value.paint}
+								/>
+							))
+						)
+					}
+				</Group >
+			</Canvas >
+		</SafeAreaView >
+	);
+};
+
+export default App;
